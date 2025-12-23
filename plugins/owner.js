c9fd7368411283bc5a72b6fe09f9345c03da95e0
rdxsigma <abulwahabkhan76@gmail.com>
2025-12-23 22:25:45 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -2,27 +2,27 @@ const { cmd } = require('../command');
 const config = require('../config');
 
 cmd({
-    pattern: "owner",
+    pattern: "Abdulwahab",
     react: "✅", 
-    desc: "Get owner number",
+    desc: "923449907936",
     category: "main",
     filename: __filename
 }, 
 async (conn, mek, m, { from }) => {
     try {
-        const ownerNumber = config.OWNER_NUMBER; // Fetch owner number from config
-        const ownerName = config.OWNER_NAME;     // Fetch owner name from config
+        const ownerNumber = config.923449907936; // Fetch owner number from config
+        const ownerName = config.Abdulwahab;     // Fetch owner name from config
 
         const vcard = 'BEGIN:VCARD\n' +
                       'VERSION:3.0\n' +
-                      `FN:${ownerName}\n` +  
-                      `TEL;type=CELL;type=VOICE;waid=${ownerNumber.replace('+', '')}:${ownerNumber}\n` + 
+                      `FN:${Abdulwahab}\n` +  
+                      `TEL;type=CELL;type=VOICE;waid=${923449907936.replace('+', '')}:${ownerNumber}\n` + 
                       'END:VCARD';
 
         // Send the vCard
         const sentVCard = await conn.sendMessage(from, {
             contacts: {
-                displayName: ownerName,
+                displayName: Abdulwahab,
                 contacts: [{ vcard }]
             }
         });
@@ -30,22 +30,22 @@ async (conn, mek, m, { from }) => {
         // Send the owner contact message with image and audio
         await conn.sendMessage(from, {
             image: { url: 'https://files.catbox.moe/yj7zp0.png' }, // Image URL from your request
-            caption: `╭━━〔 *𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃* 〕━━┈⊷
+            caption: `╭━━〔 *Rdxsigma* 〕━━┈⊷
 ┃◈╭─────────────·๏
 ┃◈┃• *Here is the owner details*
-┃◈┃• *Name* - ${ownerName}
-┃◈┃• *Number* ${ownerNumber}
+┃◈┃• *Name* - ${RDXSIGMA}
+┃◈┃• *Number* ${923449907936}
 ┃◈┃• *Version*: 2.0.0 Beta
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
-> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃 ❣️*`, // Display the owner's details
+> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ RDXSIGMA_𝐌𝐃 ❣️*`, // Display the owner's details
             contextInfo: {
-                mentionedJid: [`${ownerNumber.replace('+', '')}@s.whatsapp.net`], 
+                mentionedJid: [`${Rdxsigma.replace('+', '')}@s.whatsapp.net`], 
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: '*𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃*',
+                    newsletterJid: '923449907936@s.whatsapp.net',
+                    newsletterName: '*RDXSIGMA𝐌𝐃*',
                     serverMessageId: 143
                 }            
             }
